e7351d706cb71e5929ea0989b859319bdce95088
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2020-05-04 21:39:05 +0300

    revpbuf: Update core module

--- revpbuf/core.py
+++ revpbuf/core.py
@@ -1,12 +1,22 @@
 import io
 
+from enum import Enum
 from collections import namedtuple
-from typing import Iterable, Optional
+from typing import Iterable, Optional, Union
 
 # Core parsing. This handles the most low-level deserialization.
 # No guessing going on here. These functions return None on EOF.
 
 ProtoId = namedtuple("ProtoId", ["field_no", "wire_type"])
+
+
+class WireType(Enum):
+    Varint = 0
+    Fixed64 = 1
+    LengthDelimited = 2
+    StartGroup = 3
+    EndGroup = 4
+    Fixed32 = 5
 
 
 def _iter_bytes(stream: io.BufferedIOBase) -> Iterable[bytes]:
@@ -49,29 +59,39 @@ def read_identifier(stream: io.BufferedIOBase) -> Optional[ProtoId]:
     return ProtoId(identifier >> 3, identifier & 0b111)
 
 
-def read_value(file, wire_type):
-    if wire_type == 0:
-        return read_varint(file)
-    elif wire_type == 1:
-        c = file.read(8)
-        if not len(c):
-            return None
-        assert (len(c) == 8)
-        return c
-    elif wire_type == 2:
-        length = read_varint(file)
-        if length is None:
-            return None
-        c = file.read(length)
-        assert (len(c) == length)
-        return io.BytesIO(c)
-    elif wire_type == 3 or wire_type == 4:
+def read_fixed(stream: io.BufferedIOBase, wire_type: int) -> Optional[int]:
+    fixed_width = {WireType.Fixed32: 4, WireType.Fixed64: 8}
+    data = stream.read1(fixed_width[WireType(wire_type)])
+
+    if len(data) == 0 or len(data) != fixed_width[wire_type]:
+        return None
+
+    return int.from_bytes(data, byteorder="little")
+
+
+def read_length_delimited(stream: io.BufferedIOBase) -> Optional[bytes]:
+    length = read_varint(stream)
+
+    if length is None:
+        return None
+
+    data = stream.read1(length)
+    assert len(data) == length
+
+    return data
+
+
+def read_value(stream: io.BufferedIOBase,
+               wire_type: int) -> Optional[Union[int, bytes]]:
+    wire_type = WireType(wire_type)
+
+    if wire_type == WireType.Varint:
+        return read_varint(stream)
+    elif wire_type == WireType.Fixed32 or wire_type == WireType.Fixed64:
+        return read_fixed(stream, wire_type)
+    elif wire_type == WireType.LengthDelimited:
+        return read_length_delimited(stream)
+    elif wire_type == WireType.StartGroup or wire_type == WireType.EndGroup:
         assert True
-    elif wire_type == 5:
-        c = file.read(4)
-        if len(c) == 0:
-            return None
-        assert (len(c) == 4)
-        return c
 
     raise Exception("Unknown wire type %d" % wire_type)
